ad771bdd650ad930e2ac54b3f268ad4d80fdbfc1
Christian Zenaty <cazenaty@yahoo.com>
2018-03-20 22:49:11 -0700

    updated home layout and styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about-container">
-      <p>All the cool things that I can do</p>
+      <p>All the cool things that I can do</p>      
     </div>
   );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "./Home.css";
-import FaProfile from "react-icons/lib/md/account-circle";
 import About from "../About/About";
-
+import pic from "../../Assets/DSCF0210 copy-3264 x 4896-2.jpg";
+import pic2 from "../../Assets/profile-pic4x3.jpg";
+import pic3 from "../../Assets/profile-pic-400x400.jpg";
+import pic4 from "../../Assets/profile-pic4x4.jpg";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="profile-img-wrapper">
-        <FaProfile />
+      <div className="profile-container">
+        <div className="profile-img-wrapper">
+          {/* <FaProfile /> */}
+          <img className="headshot" src={pic4} alt="Christian Zenaty" />
+        </div>
+        <div className="greeting-wrapper">
+          <h1>Hey, I'm Christian</h1>
+          <p>A front-end Web Developer based in the San Francisco Bay Area</p>
+        </div>
       </div>
-      <h1>Hey, I'm Christian</h1>
-      <p>A front-end Web Developer based in the San Francisco Bay Area</p>
       <hr />
-      <div>
-        <About />
-      </div>
+      <About />
     </div>
   );
 };
