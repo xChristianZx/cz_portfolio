88b38c30a3c8a70c058825bcf136c9ef36b34b99
Christian Zenaty <cazenaty@yahoo.com>
2018-03-22 15:15:50 -0700

    adding projects list

---
+++ src/Assets/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    name: "crypTick",
+    src:
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1521752295/project_thumbnails/crypTick.png",
+    tags: "reactjs, websockets, expressjs",
+    link: "https://cryptick.herokuapp.com/"
+  },
+  {
+    name: "Chart It",
+    src:
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1521752294/project_thumbnails/ChartTheMarket.png",
+    tags: "reactjs, websockets, expressjs",
+    link: "https://chart-it-z.herokuapp.com/"
+  },
+  {
+    name: "VoterZ",
+    src:
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1521752279/project_thumbnails/voting_app.png",
+    tags: "reactjs, websockets, expressjs",
+    link: "https://voterz.herokuapp.com/"
+  },
+  {
+    name: "Momentum Clone",
+    src:
+      "http://res.cloudinary.com/zenpicscloud/image/upload/c_scale,q_20,w_1000/v1521754421/MomentumClone.png",
+    tags: "reactjs, sass",
+    link: "https://sparkling-substance.surge.sh"
+  },
+  {
+    name: "Calculator",
+    src:
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1521755824/project_thumbnails/Calculator.png",
+    tags: "jquery, sass, css grid",
+    link: "https://codepen.io/christianZ/full/JJWmox"
+  },
+  {
+    name: "Simon",
+    src:
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1521755825/project_thumbnails/Simon.png",
+    tags: "jquery",
+    link: "https://codepen.io/christianZ/pen/BdBvmx"
+  }
+];
+
+export default projects;
