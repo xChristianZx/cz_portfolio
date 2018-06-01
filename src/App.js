efcca057fb34d5f7301ad7416c9fde35f3987756
Christian Zenaty <cazenaty@yahoo.com>
2018-06-01 14:13:51 -0700

    adding 404 component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 // import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Footer from "./components/Footer/Footer";
+
+const NoMatch = () => {
+  return (
+    <div>
+      <h1>Sorry, this page doesn't exist</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -14,9 +22,12 @@ class App extends Component {
         <div className="router-wrapper">
           <NavBar />
           <div className="content-container">
-            {/* <Route exact path="/" component={Landing} /> */}
-            <Route exact path="/" component={Home} />
-            <Route path="/portfolio" component={Portfolio} />
+            <Switch>
+              {/* <Route exact path="/" component={Landing} /> */}
+              <Route exact path="/" component={Home} />
+              <Route path="/portfolio" component={Portfolio} />
+              <Route component={NoMatch} />
+            </Switch>
           </div>
           <Footer />
         </div>
