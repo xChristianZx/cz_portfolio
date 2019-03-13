03c428c2016cc413ec952eab3f4c66ba6b15bbb7
Christian Zenaty <cazenaty@yahoo.com>
2019-03-12 17:26:34 -0700

    Update project list

--- src/Assets/projects.js
+++ src/Assets/projects.js
@@ -1,12 +1,21 @@
 const projects = [
   {
-    name: "uNight",
+    name: "Founder Connect",
     src:
-      "https://res.cloudinary.com/zenpicscloud/image/upload/q_50/v1524176533/uNight.png",
+      "https://res.cloudinary.com/zenpicscloud/image/upload/v1550192378/project_thumbnails/founder_connect_2.png",
+    tags: "reactjs, redux, react-router, nodejs, express, mongodb",
+    link: "https://connect-founder.herokuapp.com/",
+    github: "https://github.com/chingu-voyage6/Bears-Team-02",
+    description: "A professional networking app to connect technical and non-technical founders"
+  },
+  {
+    name: "uNight",
+    src: "https://res.cloudinary.com/zenpicscloud/image/upload/q_35/v1524176533/project_thumbnails/uNight.png",
     tags: "reactjs, redux, react-router, nodejs, express, mongodb",
     link: "https://unight-tonight.herokuapp.com/",
     github: "https://github.com/xChristianZx/nightlife_app",
-    description:"A nightlife coordination app that allows users to search for bars within an area and allows signed-in users to RSVP to particular bars"
+    description:
+      "A nightlife coordination app that allows users to search for bars within an area and allows signed-in users to RSVP to particular bars"
   },
   {
     name: "crypTick",
@@ -15,7 +24,8 @@ const projects = [
     tags: "reactjs, websockets/ws, expressjs",
     link: "https://cryptick.herokuapp.com/",
     github: "https://github.com/xChristianZx/crypTick",
-    description:"A real-time price quoting toolbar for cryptocurrencies utilizing data from GDAX exchange API and emitting updates to connected clients via websockets "
+    description:
+      "A real-time price quoting toolbar for cryptocurrencies utilizing data from GDAX exchange API and emitting updates to connected clients via websockets "
   },
   {
     name: "Chart It",
@@ -24,7 +34,8 @@ const projects = [
     tags: "reactjs, websockets/ws, expressjs, mongoDB",
     link: "https://chart-it-z.herokuapp.com/",
     github: "https://github.com/xChristianZx/stock_charting_app",
-    description:"A full-stack stock charting app that emits a centralized watchlist in real-time to all clients"
+    description:
+      "A full-stack stock charting app that emits a centralized watchlist in real-time to all clients"
   },
   {
     name: "VoterZ",
@@ -33,7 +44,8 @@ const projects = [
     tags: "expressjs, mongoDB, ejs",
     link: "https://voterz.herokuapp.com/",
     github: "https://github.com/xChristianZx/voting_app",
-    description:"A full-stack voting app that allows non-authenticated users to vote on different polls and allows authenticated users to manage polls they create "
+    description:
+      "A full-stack voting app that allows non-authenticated users to vote on different polls and allows authenticated users to manage polls they create "
   },
   {
     name: "Momentum Clone",
@@ -42,7 +54,7 @@ const projects = [
     tags: "reactjs, sass",
     link: "https://sparkling-substance.surge.sh",
     github: "https://github.com/xChristianZx/momentum_clone",
-    description:"A clone of the popular browser extension"
+    description: "A clone of the popular browser extension"
   },
   {
     name: "Calculator",
@@ -50,9 +62,8 @@ const projects = [
       "https://res.cloudinary.com/zenpicscloud/image/upload/v1521755824/project_thumbnails/Calculator.png",
     tags: "jquery, sass, css grid",
     link: "https://codepen.io/christianZ/full/JJWmox",
-    github:
-      "https://github.com/xChristianZx/FCC2/tree/master/Projects/calculator",
-      description:"A Simple calculator"
+    github: "https://github.com/xChristianZx/FCC2/tree/master/Projects/calculator",
+    description: "A Simple calculator"
   },
   {
     name: "Simon",
@@ -61,7 +72,7 @@ const projects = [
     tags: "jquery",
     link: "https://codepen.io/christianZ/pen/BdBvmx",
     github: "https://github.com/xChristianZx/FCC2/tree/master/Projects/simon",
-    description:"A clone of the Simon Game"
+    description: "A clone of the Simon Game"
   }
 ];
 
